831e4378026d8e77281f7ed6bbbf8ada98ed8ef7
Project_Account62 <jwenpaq@gmail.com>
2022-02-10 10:48:35 +0800

    added a bunch of comments in main activity file for possible design ideas ig

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -7,6 +7,15 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        // hi!
+        /**
+         * I'd say that on card click, we can probably do a thing very similar to the ICA, right?
+         * Preferably we also make a "return to previous screen button" right there as well, without needing to click at the top left corner? For convenience, that is.
+         * To be honest I think that it's better for us not to cram the initial card with details.
+         * Instead, we can just put all the necessary information in the after screen. (Maybe we can keep the image, name, and price, and leave the rest to the next screen.)
+         *
+         * For customer reviews, do you also want to do a similar card thing?
+         * It feels like the organisation can work in a very similar way ~~so we can just copy the code over~~
+         * And yes, the new user sign-up part isn't hard, I guess? It's just Y3 CS stuff I assume.
+         */
     }
 }
