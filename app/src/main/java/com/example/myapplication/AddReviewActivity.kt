2f86879c8bc51a547d9867248b31204cb8b45a25
Project_Account62 <jwenpaq@gmail.com>
2022-02-19 17:43:23 +0800

    merged the stuff

--- app/src/main/java/com/example/myapplication/AddReviewActivity.kt
+++ app/src/main/java/com/example/myapplication/AddReviewActivity.kt
@@ -7,6 +7,7 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.RatingBar
+import com.google.android.material.snackbar.Snackbar
 
 class AddReviewActivity : AppCompatActivity() {
     lateinit var button: Button
@@ -24,11 +25,22 @@ class AddReviewActivity : AppCompatActivity() {
         ratingBar.stepSize = 0.5f
 
         button.setOnClickListener { view ->
-            val intent = Intent(this, ReviewActivity::class.java)
-            intent.putExtra("newReview", true)
-            intent.putExtra("rating", ratingBar.rating.toString())
-            intent.putExtra("review", editTextReview.text.toString())
-            this.startActivity(intent)
+            val ratingVal = ratingBar.rating
+            val review = editTextReview.text.toString()
+            if (review.isEmpty()) {
+                Snackbar.make(view, "You have not put in a review", Snackbar.LENGTH_LONG)
+                    .show()
+            } else if (ratingVal == 0.0F) {
+                Snackbar.make(view, "You have not put in your rating", Snackbar.LENGTH_LONG)
+                    .show()
+            } else {
+                val intent = Intent(this, ReviewActivity::class.java)
+                intent.putExtra("newReview", true)
+                intent.putExtra("rating", ratingBar.rating.toString())
+                intent.putExtra("review", editTextReview.text.toString())
+                this.startActivity(intent)
+            }
+
         }
     }
 
